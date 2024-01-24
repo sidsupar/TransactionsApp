5a67ab7063bfcd8b118403911728cd21460f8f59
sidsupar <siddharthamudgal99@gmail.com>
2024-01-24 17:31:13 +0530

    Backend added with all basic functionalities

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -35,6 +35,7 @@ router.post("/transfer",async (req, res)=>{
     //Retreiving user info from request jwt
     const token = req.headers.authorization.trim().split(" ")[1];
     const username = jwt.decode(token).username;
+
     //Start the transaction
     const session = await mongoose.startSession();
     session.startTransaction();
@@ -42,22 +43,18 @@ router.post("/transfer",async (req, res)=>{
         //Get to and amount from request body
         const to = req.body.to;
         const amount = req.body.amount;
+
         //Search for the user and the user to which the amount has to be transferred in DB
-        const user1 = await User.findOne({username:username});        
-        const user2 = await User.findOne({username:to});        
-        console.log("Inside Transfer ------------------------------");
-        console.log(`user1:`);
-        console.log(user1);
-        console.log(`user2:`);
-        console.log(user2);
-        console.log("Inside Transfer ------------------------------")
-        //Check both the users exists in DB
-        if(user1 != null && user2 != null){
-            console.log("Inside user accounts check")
+        const user1 = await User.findOne({username:username}).session(session);        
+        const user2 = await User.findOne({username:to}).session(session);        
+
+        if(!(user1 == null || user2 == null)){
             const userFrom = user1["_doc"]["_id"];
             const userTo = user2["_doc"]["_id"];
+
             //Get the user account from which the transaction has to occur
-            const userFromAccount = await Account.findOne({userId:userFrom});
+            const userFromAccount = await Account.findOne({userId:userFrom}).session(session);
+
             //Checking the user's balance
             if(userFromAccount["_doc"].balance >= parseInt(amount)){
                 //Initate transaction
@@ -65,10 +62,10 @@ router.post("/transfer",async (req, res)=>{
                 await Account.updateOne({userId:userTo},{$inc:{balance:parseInt(amount)}}).session(session);
             }else{
                 //Abort transaction if balance is low
-                await session.abortTransaction();
                 throw new Error("Low balance!!");
             }
             await session.commitTransaction();
+
             //Getting user balance from DB
             const userAfterTransfer = await Account.findOne({userId:userFrom});
             res.status(200).json({
@@ -77,13 +74,14 @@ router.post("/transfer",async (req, res)=>{
             })
         }else{
             //Abort transaction if user not found
-            await session.abortTransaction();
             res.status(411).json({
                 msg:`Not able to transfer amount from ${username} to ${to}`,
                 why:"Invalid account ID"
-            })
+            });
+            await session.abortTransaction();
         }
     }catch(err){
+        await session.abortTransaction();
         res.status(503).json({
             msg:"Something went wrong while transferring money",
             err:err.message
